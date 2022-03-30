cd219e7b70497b0fc877d64ba7bcf7350a0dfc3e
= <=>
2022-03-30 14:27:13 -0500

    completed

---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction Time: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null
+    }
+  },
+  mounted() {
+    if (this.score <= 250) {
+      this.rank = "Lightening Fast"
+    } else if (this.score <= 400) {
+      this.rank = "Rapid Reflexes"
+    } else {
+      this.rank = "Grab a Red Bull!!"
+    }
+  }
+}
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: darkslateblue;
+  font-weight: bold;
+}
+</style>
